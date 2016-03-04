3d9ba77868a4c89dae04991d51bfe41426aa76b2
awatson32 <audrey.watson32@gmail.com>
2016-03-04 09:05:36 -0600

    Added more to reusableTemp

--- reusable_assignment/reusableTemplate.js
+++ reusable_assignment/reusableTemplate.js
@@ -1,29 +1,115 @@
 var library = (function(){
 	return {
 		// Utility --- Complete Functions Below
-		identity : function(val) {},
+		identity : function(val) {
+            return val;
+        },
 
 		// Collections --- Complete Functions Below
-		each : function(list, iterator) {},
+		each : function(list, iterator) {
+            if(list.constructor === Array) {				
+                for (var i = 0; i < list.length; i++) {
+                    iterator(list[i], i, list);
+                }
+			} else {
+                for (var index = 0; index < Object.keys(list).length; index++) {
+                    var key = Object.keys(list)[index];
+                    iterator(list[key], key, list);
+                }
+            } 
+        },
 
-		filter : function(list, test) {},
+		filter : function(list, test) {
+            var arr = [];
+            for (var index = 0; index < list.length; index++) {
+                if(test(list[index])) {
+				    arr.push(list[index]);
+				}
+            }
+            return arr;
+        },
 
-		reject : function(list, test) {},
+		reject : function(list, test) {
+            var arr = [];
+            for (var index = 0; index < list.length; index++) {
+                if(!test(list[index])) {
+				    arr.push(list[index]);
+				}
+            }
+            return arr;
+        },
 
-		map : function(list, iterator) {},
+		map : function(list, iterator) {
+            var arr = [];
+            for (var index = 0; index < list.length; index++) {
+                var element = iterator(list[index]);
+                arr.push(element);
+            }
+            return arr;
+        },
 
 		pluck : function(list, key) {
 			return this.map(list, function(item){
 				return item[key];
 			});
 		},
-		reduce : function(list, iterator, accumulator) {},
+		reduce : function(list, iterator, accumulator) {
+            if (accumulator == undefined) {
+				accumulator = list[0];
+			}
+			for (var index = 0; index < list.length; index++) {
+				accumulator = iterator(accumulator, list[index]);
+				}
+				return accumulator;
+        },
 
-		every : function(list, iterator) {},
+		every : function(list, iterator) {
+            if (list.length == 0) {
+				return true;
+			}
+			if (iterator == undefined) {
+				iterator = this.identity;
+			}
+			for (var index = 0; index < list.length; index++) {
+				if(!iterator(list[index])) {
+				return false;
+				}		
+			}
+			return true;
+        },
 
-		some : function(list, iterator) {},
+		some : function(list, iterator) {
+            if (list.length == 0) {
+				return false;
+			}
+			if (iterator == undefined) {
+				iterator = this.identity;
+			}
+			for (var index = 0; index < list.length; index++) {
+				if(iterator(list[index])) {
+					return true;
+				}
+			}
+			return false;
+        },
 
-		contains : function(list, target) {},
+		contains : function(list, target) {
+            if (list.constructor === Array) {
+			for (var index = 0; index < list.length; index++) {
+				if (list[index] == target){
+					return true;
+				}
+			}
+			} else {
+				for (var index = 0; index < Object.keys(list).length; index++) {
+				var key = Object.keys(list)[index];
+				if (list[key] == target) {
+					return true;
+				}
+				}
+			}
+			return false;
+        },
 
 		// Advanced Collections --- Complete Functions Below
 		shuffle : function(array) {},
